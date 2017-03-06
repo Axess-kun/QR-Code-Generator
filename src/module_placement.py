4b9a64ccf026117f24d854ac872a30b4fe1cdf78
Axess <axess.kun@gmail.com>
2017-03-07 02:21:27 +0900

    Add alignment patterns

--- src/module_placement.py
+++ src/module_placement.py
@@ -1,5 +1,49 @@
 from PIL import Image, ImageDraw
 
+AlignmentPosition = [
+    # Each line represent each version from 1 to 40 respectively
+    [],
+    [6, 18],
+    [6, 22],
+    [6, 26],
+    [6, 30],
+    [6, 34],
+    [6, 22, 38],
+    [6, 24, 42],
+    [6, 26, 46],
+    [6, 28, 50],
+    [6, 30, 54],
+    [6, 32, 58],
+    [6, 34, 62],
+    [6, 26, 46, 66],
+    [6, 26, 48, 70],
+    [6, 26, 50, 74],
+    [6, 30, 54, 78],
+    [6, 30, 56, 82],
+    [6, 30, 58, 86],
+    [6, 34, 62, 90],
+    [6, 28, 50, 72, 94],
+    [6, 26, 50, 74, 98],
+    [6, 30, 54, 78, 102],
+    [6, 28, 54, 80, 106],
+    [6, 32, 58, 84, 110],
+    [6, 30, 58, 86, 114],
+    [6, 34, 62, 90, 118],
+    [6, 26, 50, 74, 98, 122],
+    [6, 30, 54, 78, 102, 126],
+    [6, 26, 52, 78, 104, 130],
+    [6, 30, 56, 82, 108, 134],
+    [6, 34, 60, 86, 112, 138],
+    [6, 30, 58, 86, 114, 142],
+    [6, 34, 62, 90, 118, 146],
+    [6, 30, 54, 78, 102, 126, 150],
+    [6, 24, 50, 76, 102, 128, 154],
+    [6, 28, 54, 80, 106, 132, 158],
+    [6, 32, 58, 84, 110, 136, 162],
+    [6, 26, 54, 82, 110, 138, 166],
+    [6, 30, 58, 86, 114, 142, 170]
+]
+
 class Module:
     #------------------------------
     # Converter: Convert Integer to RGB/RGBA as tuple
@@ -11,18 +55,19 @@ class Module:
         return ((value >> 24) & 0xFF, (value >> 16) & 0xFF, (value >> 8) & 0xFF, value & 0xFF)
 
     def __init__(self, version):
+        self.version = version
         #------------------------------
         # Construct QR
         #------------------------------
         self.size = (((version-1)*4)+21)
 
         # Grey BG for debugging
-        bg = self.int2rgb(0x888888)
+        self.bg = self.int2rgb(0x888888)
 
         # 2D Array
-        self.modules = [bg] * self.size
+        self.modules = [self.bg] * self.size
         for row in range(self.size):
-            self.modules[row] = [bg] * self.size
+            self.modules[row] = [self.bg] * self.size
 
         #------------------------------
         # Paint Patterns
@@ -31,9 +76,11 @@ class Module:
         self.paintFinderSeparatorPattern(0, 0)
         self.paintFinderSeparatorPattern(self.size - 7, 0)
         self.paintFinderSeparatorPattern(0, self.size - 7)
+        # Alignment Patterns
+        self.paintAlignmentPattern()
 
         # Blank Canvas
-        canvas = Image.new("RGB", (self.size, self.size), bg)
+        canvas = Image.new("RGB", (self.size, self.size), self.bg)
         
         # Put data into each module
         for i in range(len(self.modules)):
@@ -74,6 +121,40 @@ class Module:
                     # White
                     self.modules[row + r][col + c] = self.int2rgb(0xFFFFFF)
 
+    #------------------------------
+    # Alignment Patterns
+    #------------------------------
+    def paintAlignmentPattern(self):
+        # All possible position
+        pos = AlignmentPosition[self.version - 1]
+
+        # Loop for each pair
+        for i in range(len(pos)):
+            for j in range(len(pos)):
+                # Current row & column (middle of alignment pattern to be placed)
+                row = pos[i]
+                col = pos[j]
+
+                # Check if finder pattern placed -> Nothing to do with this position
+                if any([(a != b) for a, b in zip(self.modules[row][col], self.bg)]):
+                    continue
+
+                # Else, loop for drawing
+                for r in range(-2, 3):
+                    for c in range(-2, 3):
+                        if (r == -2 or r == 2 # Vertical Border
+                            or
+                            c == -2 or c == 2 # Horizontal Border
+                            or
+                            (r == 0 and c == 0) # Middle
+                            ):
+                            # Black
+                            self.modules[row + r][col + c] = self.int2rgb(0)
+                        else:
+                            # White
+                            self.modules[row + r][col + c] = self.int2rgb(0xFFFFFF)
+
     
 Module(1)
 Module(18)
+Module(40)
