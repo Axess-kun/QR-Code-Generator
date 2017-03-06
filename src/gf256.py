1e87cc5744a2a013e7d969d136d7b3273273c43e
Axess <axess.kun@gmail.com>
2017-03-06 16:34:28 +0900

    Polynomial modulation using Long division & Generator Polynomial

--- src/gf256.py
+++ src/gf256.py
@@ -42,12 +42,19 @@ class Polynomial:
     #   coefficients: list of coefficients, NOT alpha notations
     #     eg. a3*x2 + a27*x1 + a0 --> [8,12,1]
     def __init__(self, coefficients: list):
+        # Offset to kill first(front) term(s) that coefficient is 0 until it not
+        offset = 0
+        for i in range(len(coefficients)):
+            if coefficients[i] != 0:
+                break;
+            offset += 1
+
         # Create blank list
-        self.coefficients = [0] * len(coefficients)
+        self.coefficients = [0] * (len(coefficients) - offset)
 
         # Copy to class' member
-        for i in range(len(coefficients)):
-            self.coefficients[i] = coefficients[i]
+        for i in range(len(coefficients) - offset):
+            self.coefficients[i] = coefficients[i + offset]
 
     def __mul__(self, other):
         # Create blank list
@@ -70,3 +77,43 @@ class Polynomial:
 #e = c*d
 #print(e.coefficients) # should be 1,7,14,8
 #print('---------')
+
+    def __mod__(self, other):
+        diff = len(self.coefficients) - len(other.coefficients)
+        # Find remainder
+        # If len(dividend) < len(divisor), then return dividend itself because can't divide it
+        # ... used as recursive break
+        if diff < 0:
+            return self
+
+        firstTermExpDiff = int2exp(self.coefficients[0]) - int2exp(other.coefficients[0])
+
+        # Create blank list * Copy
+        coef = self.coefficients[:]
+        
+        for i, (item, otherItem) in enumerate(zip(self.coefficients, other.coefficients)):
+            coef[i] = item ^ exp2int(int2exp(otherItem) + firstTermExpDiff)
+
+        # Recursive until can't divide
+        return Polynomial(coef) % other
+
+    def getGenerator(n:int):
+        # Create a0 = 1 as initial polynomial
+        poly = Polynomial([1])
+        for i in range(n):
+            poly = poly * Polynomial([1, exp2int(i)])
+        return poly
+
+        
+
+a = Polynomial([1,7,14,8])
+b = Polynomial([1,3,2])
+c = a%b
+print(c.coefficients) # Should be 0
+print('---------')
+#d = Polynomial([32, 91, 11, 120, 209, 114, 220, 77, 67, 64, 236, 17, 236, 17, 236, 17])
+e = Polynomial.getGenerator(10) # Shoud be 1, 216, 194, 159, 111, 199, 94, 95, 113, 157, 193
+print(e.coefficients)
+#f = d%e
+#print(f.coefficients) # Should be 196, 35, 39, 119, 235, 215, 231, 226, 93, 23
+print('---------')
